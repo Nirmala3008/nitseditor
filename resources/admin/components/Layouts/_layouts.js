3de3f38339de46c90a4635de227d37f7e9f05b8c
Rubal Gulati <rubal@noetic.com>
2019-07-09 14:08:43 +0530

    Dashboard changes

--- resources/admin/components/Layouts/_layouts.js
+++
@@ -1,3 +0,0 @@
-import Vue from 'vue';
-
-Vue.component('LayoutOne', () => import('./_layout-one.vue'))
---
+++ resources/admin/components/SuperadminDashboard.vue
@@ -0,0 +1,212 @@
+<template>
+    <!--Begin::Section-->
+    <div class="row">
+        <div class="col-md-6">
+
+            <!--begin:: Widgets/Projects Sales-->
+            <div class="kt-portlet kt-portlet--bordered-semi kt-portlet--space kt-portlet--height-fluid">
+                <div class="kt-portlet__head">
+                    <div class="kt-portlet__head-label">
+                        <h3 class="kt-portlet__head-title">
+                            Projects
+                            <small>total projects</small>
+                        </h3>
+                    </div>
+                    <div class="kt-portlet__head-toolbar">
+                        <div class="dropdown dropdown-inline">
+                            <button type="button" class="btn btn-clean btn-sm btn-icon btn-icon-md" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="flaticon-more-1"></i>
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <ul class="kt-nav">
+                                    <li class="kt-nav__item">
+                                        <a href="#" class="kt-nav__link">
+                                            <i class="kt-nav__link-icon flaticon2-graph-1"></i>
+                                            <span class="kt-nav__link-text">Statistics</span>
+                                        </a>
+                                    </li>
+                                    <li class="kt-nav__item">
+                                        <a href="#" class="kt-nav__link">
+                                            <i class="kt-nav__link-icon flaticon2-layers-1"></i>
+                                            <span class="kt-nav__link-text">Reports</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="kt-portlet__body">
+                    <div class="kt-widget25">
+                        <span class="kt-widget25__stats m-font-brand">4,943</span>
+                        <span class="kt-widget25__subtitle">Total projects</span>
+                        <div class="kt-widget25__items">
+                            <div class="kt-widget25__item">
+														<span class="kt-widget25__number">
+															4,495
+														</span>
+                                <div class="progress kt-progress--sm">
+                                    <div class="progress-bar kt-bg-danger" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <span class="kt-widget25__desc">
+															Tracked projects
+														</span>
+                            </div>
+                            <div class="kt-widget25__item">
+														<span class="kt-widget25__number">
+															4,751
+														</span>
+                                <div class="progress m-progress--sm">
+                                    <div class="progress-bar kt-bg-success" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <span class="kt-widget25__desc">
+															Verified Projects
+														</span>
+                            </div>
+                            <div class="kt-widget25__item">
+														<span class="kt-widget25__number">
+															1,043
+														</span>
+                                <div class="progress m-progress--sm">
+                                    <div class="progress-bar kt-bg-warning" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <span class="kt-widget25__desc">
+															Added last 60 days
+														</span>
+                            </div>
+                        </div>
+                        <div class="kt-widget25__items">
+                            <div class="kt-widget25__item">
+														<span class="kt-widget25__number">
+															10,86,457
+														</span>
+                                <div class="progress kt-progress--sm">
+                                    <div class="progress-bar kt-bg-danger" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <span class="kt-widget25__desc">
+															Project Values(INR-Crore)
+														</span>
+                            </div>
+                            <div class="kt-widget25__item">
+														<span class="kt-widget25__number">
+															4,103
+														</span>
+                                <div class="progress m-progress--sm">
+                                    <div class="progress-bar kt-bg-success" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <span class="kt-widget25__desc">
+															Million-SqFt
+														</span>
+                            </div>
+                            <div class="kt-widget25__item">
+														<span class="kt-widget25__number">
+															1,798
+														</span>
+                                <div class="progress m-progress--sm">
+                                    <div class="progress-bar kt-bg-warning" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <span class="kt-widget25__desc">
+															Updated last 60 days
+														</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--end:: Widgets/Projects Sales-->
+        </div>
+        <div class="col-md-6">
+
+            <!--begin:: Widgets/Companies Sales-->
+            <div class="kt-portlet kt-portlet--bordered-semi kt-portlet--space kt-portlet--height-fluid">
+                <div class="kt-portlet__head">
+                    <div class="kt-portlet__head-label">
+                        <h3 class="kt-portlet__head-title">
+                            Networks
+                            <small>total connects</small>
+                        </h3>
+                    </div>
+                    <div class="kt-portlet__head-toolbar">
+                        <div class="dropdown dropdown-inline">
+                            <button type="button" class="btn btn-clean btn-sm btn-icon btn-icon-md" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
+                                <i class="flaticon-more-1"></i>
+                            </button>
+                            <div class="dropdown-menu dropdown-menu-right">
+                                <ul class="kt-nav">
+                                    <li class="kt-nav__item">
+                                        <a href="#" class="kt-nav__link">
+                                            <i class="kt-nav__link-icon flaticon2-graph-1"></i>
+                                            <span class="kt-nav__link-text">Statistics</span>
+                                        </a>
+                                    </li>
+                                    <li class="kt-nav__item">
+                                        <a href="#" class="kt-nav__link">
+                                            <i class="kt-nav__link-icon flaticon2-layers-1"></i>
+                                            <span class="kt-nav__link-text">Reports</span>
+                                        </a>
+                                    </li>
+                                </ul>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <div class="kt-portlet__body">
+                    <div class="kt-widget25">
+                        <span class="kt-widget25__stats m-font-brand">14,495</span>
+                        <span class="kt-widget25__subtitle">Industry Networks</span>
+                        <div class="kt-widget25__items">
+                            <div class="kt-widget25__item">
+														<span class="kt-widget25__number">
+															9,242
+														</span>
+                                <div class="progress kt-progress--sm">
+                                    <div class="progress-bar kt-bg-danger" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <span class="kt-widget25__desc">
+															Companies
+														</span>
+                            </div>
+                            <div class="kt-widget25__item">
+														<span class="kt-widget25__number">
+															8,147
+														</span>
+                                <div class="progress m-progress--sm">
+                                    <div class="progress-bar kt-bg-success" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <span class="kt-widget25__desc">
+															Professionals
+														</span>
+                            </div>
+                            <div class="kt-widget25__item">
+														<span class="kt-widget25__number">
+															1,043
+														</span>
+                                <div class="progress m-progress--sm">
+                                    <div class="progress-bar kt-bg-warning" role="progressbar" style="width: 65%;" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100"></div>
+                                </div>
+                                <span class="kt-widget25__desc">
+															Added last 60 days
+														</span>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <!--end:: Widgets/Companies Sales-->
+        </div>
+    </div>
+
+    <!--End::Section-->
+</template>
+
+<script>
+    export default {
+        name: "SuperadminDashboard"
+    }
+</script>
+
+<style scoped>
+
+</style>
